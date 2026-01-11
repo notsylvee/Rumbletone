126385757499b68ec4994a86828efaccb85240ff
sylv <79902941+notsylvee@users.noreply.github.com>
2026-01-11 03:20:38 -0500

    +answers.js

--- src/events/answers.js
+++ src/events/answers.js
@@ -1,40 +1,26 @@
-// module.exports = {
-//     name: "messageCreate",
-//     async execute(message) {
-//       if (message.author.bot || !message.guild) return;
+module.exports = {
+    name: "messageCreate",
+    async execute(message) {
+      if (message.author.bot || !message.guild) return;
   
-//       const msg = [
-//         `yes`,
-//         `no`,
-//         `mayyybe`,
-//         `idk`,
-//         `maybe`,
-//         `perhaps`,
-//         `perchance`,
-//         `mayhaps`,
-//         `nuh uh`,
-//         `yeah`,
-//         `nah`,
-//         `NO!`,
-//         `i dont know`,
-//         `yuh huh`,
-//         `YASSSS`
-//       ];
-//       const response = msg[Math.floor(Math.random() * msg.length)];
-//       const faces = [` `, ` `, ` `, ` `, ` `, ` `, ` `, ` `, ` `,
-//         `:3`,
-//         `>.<`,
-//         `>:(`,
-//         `uwu`,
-//         `:c`,
-//         `;3`,
-//         `>:3`,
-//         `:<`,
-//         `:>`,
-//       ]
-//       const face = faces[Math.floor(Math.random() * faces.length)];
-  
-//       if (message.content.toLowerCase().startsWith("sylvbot") && message.content.toLowerCase().endsWith("?"))
-//         {message.channel.send(`${response} ${face}`)}
-//     },
-//   };
+      const msg = [
+        `yes`,
+        `no`,
+        `mayyybe`,
+        `idk`,
+        `maybe`,
+        `perhaps`,
+        `perchance`,
+        `mayhaps`,
+        `nuh uh`,
+        `yeah`,
+        `nah`,
+        `NO!`,
+        `i dont know`,
+        `yuh huh`,
+        `YASSSS`
+      ];
+      if (message.content.toLowerCase().startsWith("rumbletone") && message.content.toLowerCase().endsWith("?"))
+        {message.channel.send(`${response}`)}
+    },
+  };
